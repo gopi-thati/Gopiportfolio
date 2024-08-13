920512975c105289e871984053db052ab9d32383
gopi thati <thatigopi27@gmail.com>
2024-08-13 16:57:16 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('nav').addClass("sticky");
+        } else {
+            $('nav').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+            
+    });
+
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $(`html`).animate({scrollTop: 0})
+    });
+});
+
+// typing animation  script
+var typed = new Typed(".typing",{
+    strings: ["Web Developer","Influencer","Freelancer"],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true
+})
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer","Influencer","Freelancer"],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true
+})
+var typed = new Typed(".typing-3",{
+    strings: ["Skills","Expriences"],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true
+})
